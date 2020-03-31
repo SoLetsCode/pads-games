63c2cfb67409abf6985032b0a06200e3fbf614cb
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-03-31 00:55:47 -0700

    Updates

---
+++ precache-manifest.014c4b2f670a94000a82500b05b013cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b5d395da1f49acb1ec05f0d6f09668",
+    "url": "/pads-games/index.html"
+  },
+  {
+    "revision": "6100e9752525ec7b77d5",
+    "url": "/pads-games/static/css/main.3a9f8524.chunk.css"
+  },
+  {
+    "revision": "1ba15bde165599b2a981",
+    "url": "/pads-games/static/js/2.756a7cac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pads-games/static/js/2.756a7cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6100e9752525ec7b77d5",
+    "url": "/pads-games/static/js/main.b2b0d0dd.chunk.js"
+  },
+  {
+    "revision": "8a1b9a663bd7c4f1ab1d",
+    "url": "/pads-games/static/js/runtime-main.6529f9dc.js"
+  },
+  {
+    "revision": "27a800dc6d31224bd95b8f51cb853d2f",
+    "url": "/pads-games/static/media/BiscuitDefault.27a800dc.jpg"
+  }
+]);

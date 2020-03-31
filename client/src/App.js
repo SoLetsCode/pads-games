5228417465ed48920b52527c990bcf4dc653d819
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-03-31 01:00:23 -0700

    template made for website need to add data and button to randomize games

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import "./styles/app.css";
+
+//components
+import Appbar from "./components/Appbar";
+import GameCard from "./components/GameCard";
+
+//Material-UI
+import { Grid } from "@material-ui/core";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Appbar />
+      <Grid container justify="center">
+        <Grid item>
+          <GameCard />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/GameCard.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import biscuitImg from "../assets/BiscuitDefault.jpg";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    margin: "0 auto 0 auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="level" className={classes.avatar}>
+            P
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Relay Race"
+      />
+      <CardMedia className={classes.media} image={biscuitImg} title="Biscuit" />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Puppy will go through a relay race and at each stop they will have to
+          do one action.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/* NOT WORKING YET<IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share"> NOT WORKING YET
+          <ShareIcon />
+        </IconButton> */}
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph variant="h6">
+            How to Play:
+          </Typography>
+          <Typography paragraph>Lay 3 objects out on the floor</Typography>
+          <Typography paragraph>
+            Go to each object and ask for one position (ie. sit, stand, down)
+          </Typography>
+          <Typography paragraph variant="h6">
+            Goal
+          </Typography>
+          <Typography paragraph>
+            To have puppy respond to cue next to the handler.
+          </Typography>
+          <Typography paragraph variant="h6">
+            Challenge Me!
+          </Typography>
+          <Typography paragraph>Sprinkle kibble between objects.</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
